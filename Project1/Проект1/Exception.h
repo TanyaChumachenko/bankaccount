f4141ca003016c63fef10121c663b6217c33ca8a
Tanya100 <svitlana0981@gmail.com>
2021-12-08 01:15:22 +0200

    Development a class of exeptions

---
+++ Project1/Проект1/Exception.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<exception>
+#include<string>
+//клас-виключення
+class CException:public std::exception
+{
+	std::string message;
+public:
+	CException(const std::string& msg);
+	//
+	//функція повертає повідомлення помилки
+	virtual const char* what() const noexcept override;
+};
+
