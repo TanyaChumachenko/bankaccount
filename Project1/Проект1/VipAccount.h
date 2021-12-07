b5000456006c023bfe28264bb097b13edf4edef2
Tanya100 <svitlana0981@gmail.com>
2021-12-08 01:44:35 +0200

    Edit VIP account class

---
+++ Project1/Проект1/VipAccount.h
@@ -0,0 +1,24 @@
+#pragma once
+#include"Account.h"
+#include "SocialAccount.h"
+#include "DefaultAccount.h"
+
+// клас з множинним наслідуванням
+class CVipAccount : public CDefaultAccount, public CSocialAccount
+{
+private:
+	double VipPercentAdd; // відсоток для нарахування на залишок (поданий у сотих): 0.01 == 1 %
+	double SumForHigherPercent; // сума, після якої нараховується	підвищений відсоток
+	double creditLimit; // ліміт на використання кредитних коштів, можливо загнати рахунок в мінус
+public:
+	// конструктор + віртуальний деструктор
+	CVipAccount();
+	virtual ~CVipAccount();
+	// віртуальні функції, які треба перевизначити
+	virtual void InputMoney(double m); // покласти на рахунок
+	virtual void PayUtilities(double m);// оплата комунальних послуг
+	virtual void AddPercents(); // нарахування відсотків з залишку	на карті
+	virtual void OutputMoney(double m); // видати готівку
+};
+
+
